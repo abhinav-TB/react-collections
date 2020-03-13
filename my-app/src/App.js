a7b4bbf5c8605bcaae2d1523f8d6d0489a01440a
abhinav-TB <abhinavtb@gmail.com>
2020-03-13 21:46:36 +0530

    created ninja component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React,{Component} from 'react';
+import Ninjas from './ninjas.js';
 
-
-function App() {
+class App extends Component{
+  render(){
   return (
     <div className="App">
-     <h1>mwelcome every one </h1>
+     <h1>welcome every one </h1>
+     <Ninjas />
     </div>
   );
+}
 }
 
 export default App;
---
+++ my-app/src/ninjas.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import App from './App';
+
+
+class ninjas extends Component{
+    render(){
+        return(
+            <div className="ninjas">
+                <div>name:ryu</div>
+                <div>age:30</div>
+                <div>belt:bl</div>
+            </div>
+        )
+    }
+}
+export default ninjas;
