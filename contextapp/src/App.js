c4ab6d55bae2e33f415a4e8303ce99c72d7e1d7a
abhinav-TB <abhinavtb@gmail.com>
2020-03-22 08:30:01 +0530

    added context

--- contextapp/src/App.js
+++ contextapp/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-
+import BookList from'./components/Booklist';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <BookList/>
     </div>
   );
 }
