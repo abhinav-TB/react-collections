a3ea53234dfe68b922bce89c7f918769e7956a81
abhinav-TB <abhinavtb@gmail.com>
2020-03-22 08:35:23 +0530

    added css

--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -1,10 +1,8 @@
 import React,{Component} from 'react';
 
 class Navbar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  };
-    }
+
+    
     render() {
         return (
             <nav>
