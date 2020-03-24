2d68ebf846dca0f884e170b026bd4f028795ccec
abhinav-TB <abhinavtb@gmail.com>
2020-03-24 12:08:54 +0530

    todo app completed

--- todoapp/src/AddTodo.js
+++ todoapp/src/AddTodo.js
@@ -11,13 +11,17 @@ class AddTodo extends Component {
     handleSubmit=(e)=>{
         e.preventDefault();
         this.props.addTodo(this.state);
+        this.setState({
+            content:""
+        })
+
     } 
     render() {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <label> addnew Todo:</label>
-                    <input type="text" onChange={this.handleChange}/>
+                    <input type="text" onChange={this.handleChange} value={this.state.content}/>
                 </form>
             </div>
         );
