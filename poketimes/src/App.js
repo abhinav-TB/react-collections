c4af784112cdcf5d49e42dbffb6ed138108cbb7b
abhinav-TB <abhinavtb@gmail.com>
2020-03-25 10:18:13 +0530

    added home

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -1,9 +1,7 @@
 import React ,{Component}from 'react';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {  };
+
   }
   render() {
     return (
---
+++ poketimes/src/components/home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+const Home=()=>{
+    return(
+
+        <div>
+            <div className="container">
+                <h4 className="center"> </h4>
+                <p>Est magna Lorem sit eiusmod irure aliqua eiusmod cillum reprehenderit. Id dolore et magna sit exercitation duis adipisicing eu proident ea id sint commodo consectetur. Minim nulla veniam elit culpa ex veniam reprehenderit dolor culpa. Anim fugiat quis sint aliquip do sint nulla ipsum. Adipisicing minim excepteur cupidatat tempor ut deserunt quis.
+               </p>
+
+            </div>
+        </div>
+    )
+
+}
+export de
