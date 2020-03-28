57a3a78a9b0b2b4c5b4d64c0da0386217104c080
abhinav-TB <abhinavtb@gmail.com>
2020-03-28 19:16:16 +0530

    added home state to redux

--- poketimes/src/components/home.js
+++ poketimes/src/components/home.js
@@ -1,22 +1,12 @@
 import React,{Component} from 'react'
 import {Link} from 'react-router-dom';
-import Axios from 'axios'
 import Pokeball from '../pokeball.png';
+import {connect} from 'react-redux'
 class Home extends Component{
-    state={
-        posts:[]
-    }
-    componentDidMount(){
-        Axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(res=>{
-            console.log(res)
-            this.setState({
-                posts:res.data.slice(0,10)
-            })
-        })
-    }
+  
     render(){
-        const{posts}=this.state;
+        console.log(this.props)
+        const{posts}=this.props;
         const postList=posts.length?(posts.map(post=>{
             return(
                 <div className="post card" key={post.id}>
@@ -46,4 +36,10 @@ class Home extends Component{
     }
 
 }
-export default Home
+
+const mapStateTOProps=(state)=>{
+    return{
+        posts:state.posts
+    }
+}
+export default connect(mapStateTOProps)(Home)
---
+++ poketimes/src/reducers/rootReducer.js
@@ -0,0 +1,14 @@
+const initState={
+    posts:[
+        {id:'1',title:'squirtle laid an egg',body:'Do in duis officia commodo Lorem cupidatat quis dolore labore.'},
+        {id:'2',title:'charmaner laid an egg',body:'Do in duis officia commodo Lorem cupidatat quis dolore labore.'},
+        {id:'3',title:'a helix fossil was found',body:'Do in duis officia commodo Lorem cupidatat quis dolore labore.'}
+
+    ]
+}
+
+const rootReducer=(state=initState,action)=>{
+    return state;
+}
+
+export default rootReducer
