95559c63e39f6310ae611d9a9aaeb65251438a66
abhinav-TB <abhinavtb@gmail.com>
2020-03-29 22:05:30 +0530

    signup done

---
+++ marioplan/src/components/auth/SignUp.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+
+class SignUp extends Component {
+    state={
+        email:'',
+        password:'',
+        firstNmae:"",
+        lastName:""
+    }
+    handleChange=(e)=>{
+        this.setState({
+            [e.target.id]:e.target.value
+        })
+    }
+    handleSubmmit=(e)=>{
+        e.preventDefault();
+        console.log(this.state)
+    }
+    render() {
+        return (
+            <div className="container">
+            <form onSubmit={this.handleSubmmit} className="white">
+                <h5 className="grey-text text-darken-3">Sign In</h5>
+                <div className="input-field">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" onChange={this.handleChange}/>
+                </div>
+                <div className="input-field">
+                    <label htmlFor="password">password</label>
+                    <input type="password" id="password" onChange={this.handleChange}/>
+                </div>
+                <div className="input-field">
+                    <label htmlFor="firstName">firstName</label>
+                    <input type="text" id="firstName" onChange={this.handleChange}/>
+                </div>
+                <div className="input-field">
+                    <label htmlFor="lastName">lastName</label>
+                    <input type="text" id="lastName" onChange={this.handleChange}/>
+                </div>
+                <div className="input-field">
+                <button className="btn pink lighten-1 z-depth-0">Login</button>
+                </div>
+            </form>
+                
+            </div>
+        )
+    }
+}
+
+export default SignUp
