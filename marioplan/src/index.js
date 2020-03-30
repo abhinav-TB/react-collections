0d8ac0800b875744a3c63bdbd8c62abb7a5e5d66
abhinav-TB <abhinavtb@gmail.com>
2020-03-30 10:58:34 +0530

    auth basic setup

--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import{createStore} from 'redux'
 
+
+const store= createStore()
 ReactDOM.render(
   <React.StrictMode>
     <App />
