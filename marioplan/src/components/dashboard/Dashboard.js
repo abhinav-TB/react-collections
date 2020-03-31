77065c676295972ca198ebf66f930666c872c967
abhinav-TB <abhinavtb@gmail.com>
2020-03-31 15:12:23 +0530

    aded dummy data

--- marioplan/src/components/dashboard/Dashboard.js
+++ marioplan/src/components/dashboard/Dashboard.js
@@ -5,12 +5,13 @@ import {connect}from 'react-redux'
 class Dashboard extends Component {
   
     render() {
-        c
+        // console.log(this.props);
+        const {projects }=this.props
         return (
             <div className="dashboard container">
                <div className="row">
                   <div className="col s12 m6">
-                      <ProjectList/>
+                      <ProjectList projects={projects}/>
                       </div> 
                   <div className="col s12 m5 offset-m1">
                       <Notifications/>
@@ -26,4 +27,4 @@ const mapStateToProps=(state)=>{
         projects:state.project.projects
     }
 }
-export default connect()(Dashboard)
+export default connect(mapStateToProps)(Dashboard)
