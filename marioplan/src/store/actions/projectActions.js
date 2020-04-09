771a64c845baf3737be5814c83e415763263d1d2
abhinav-TB <abhinavtb@gmail.com>
2020-04-09 10:25:25 +0530

    initialized

--- marioplan/src/store/actions/projectActions.js
+++ marioplan/src/store/actions/projectActions.js
@@ -1,6 +1,15 @@
+
+
 export const createProject=(project)=>{
-    return(dispatch,getState)=>{
-    //    async call
-    dispatch({type:'CREATE_PROJECT',project});
+    return(dispatch,getState,{getFIrebase,getFirestore})=>{
+    const firestore=getFirestore();
+    firestore.Collection('projects').add({
+        ...project,
+        authorFirstName:'net',
+        authorLastName:"ninja",
+        authorid:"12345",
+        createdAt:new Date()
+    }).then(()=>{ dispatch({type:'CREATE_PROJECT',project})})
+     .catch((err)=>{ dispatch({type:'CREATE_PROJECT_ERROR',err})})
     }
 }
